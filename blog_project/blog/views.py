d02374dc768ab628b0db73bf7a7dee49324ecb9f
iyegbekosa <aikpitanyi.iyegbekosa@gmail.com>
2022-11-04 00:25:10 +0100

    comment views and urls

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
+
+# Post views begin here
 class AboutView(TemplateView):
     template_name = 'about.html'
     
@@ -49,3 +53,44 @@ class DraftListView(LoginRequiredMixin,ListView):
     
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+    
+    
+#Comment views begin here
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
+    
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST' :
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    
+    else:
+        form = CommentForm
+    return render(request, 'blog/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_deatail',pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_deatail',pk=post_pk)
