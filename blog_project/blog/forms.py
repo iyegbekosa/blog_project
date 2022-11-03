288384fa6430a44b5cc030aba4109bad357bfb46
iyegbekosa <aikpitanyi.iyegbekosa@gmail.com>
2022-11-03 10:54:39 +0100

    forms created and seetings.py adjusted

---
+++ blog_project/blog/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    
+    class Meta():
+        model = Post
+        fields = ('author','title','text')
+        
+        #the widget is so we can implememt styling on our form fields
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent postcontent'}),
+            
+        }
+        
+        
+class CommentForm(forms.ModelForm):
+    
+    class Meta():
+        model = Comment
+        fields = ('author','text')
+        
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea '}),
+            
+        }
